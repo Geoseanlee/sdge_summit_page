aa34aed802ab2fb32dc6a1cfc0e6bfa51f73b1a8
Drame <126373559+dpviivqb@users.noreply.github.com>
2025-07-29 00:34:47 +1000

    修复index.js (api)

--- sdgs-admin/src/api/index.js
+++ sdgs-admin/src/api/index.js
@@ -4,7 +4,7 @@ export function get(url, params) {
   return request({
     url,
     method: 'get',
-    params
+    params,
   })
 }
 
@@ -12,7 +12,7 @@ export function post(url, data) {
   return request({
     url,
     method: 'post',
-    data
+    data,
   })
 }
 
@@ -20,14 +20,14 @@ export function put(url, data) {
   return request({
     url,
     method: 'put',
-    data
+    data,
   })
 }
 
 export function del(url) {
   return request({
     url,
-    method: 'delete'
+    method: 'delete',
   })
 }
 
@@ -41,19 +41,14 @@ export function updateExpoInfo(id, data) {
   return put(`/api/expo/manage/${id}`, data)
 }
 
-// 获取世博会背景页面数据
-export function getExpoBackgroundData() {
-  return get('/api/expo/background/data')
+// 获取最新的首页信息
+export function getHomeInfo() {
+  return get('/api/home/info')
 }
 
-// 保存世博会背景页面数据
-export function saveExpoBackgroundData(data) {
-  return post('/api/expo/background/data', data)
-}
-
-// 更新世博会背景页面数据
-export function updateExpoBackgroundData(data) {
-  return put('/api/expo/background/data', data)
+// 更新首页信息
+export function updateHomeInfo(id, data) {
+  return put(`/api/home/manage/${id}`, data)
 }
 
 // 上传图片
@@ -72,3 +67,24 @@ export function listImages(params) {
 export function deleteImage(fileUrl) {
   return del(`/api/file/delete?fileUrl=${encodeURIComponent(fileUrl)}`)
 }
+
+//********************************************************************************
+//下面代码由Drame完成注意区分！！！
+//********************************************************************************
+
+// 获取世博会背景页面数据
+export function getExpoBackgroundData() {
+  return get('/api/expo/background/data')
+}
+
+// 保存世博会背景页面数据
+export function saveExpoBackgroundData(data) {
+  return post('/api/expo/background/data', data)
+}
+
+// 更新世博会背景页面数据
+export function updateExpoBackgroundData(data) {
+  return put('/api/expo/background/data', data)
+}
+
+//********************************************************************************
